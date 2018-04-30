95cb9b3c776e118967410d08c41e89a4de454654
linucksrox <ericdalyblog@dalydays.com>
2018-04-30 13:58:49 -0400

    Complete chapter 8 challenge, page 665

--- app/src/main/java/com/dalydays/android/criminalintent/Crime.java
+++ app/src/main/java/com/dalydays/android/criminalintent/Crime.java
@@ -9,6 +9,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private boolean mRequiresPolice;
 
     public Crime() {
         mId = UUID.randomUUID();
@@ -37,6 +38,14 @@ public class Crime {
 
     public void setSolved(boolean solved) {
         mSolved = solved;
+    }
+
+    public boolean isRequiresPolice() {
+        return mRequiresPolice;
+    }
+
+    public void setRequiresPolice(boolean requiresPolice) {
+        mRequiresPolice = requiresPolice;
     }
 
     public UUID getId() {
--- app/src/main/java/com/dalydays/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/dalydays/android/criminalintent/CrimeListFragment.java
@@ -64,7 +64,34 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
-    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+    private class CrimeRequiresPoliceHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private Crime mCrime;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+
+        public CrimeRequiresPoliceHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime_requires_police, parent, false));
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = itemView.findViewById(R.id.crime_title);
+            mDateTextView = itemView.findViewById(R.id.crime_date);
+        }
+
+        public void bind(Crime crime) {
+            mCrime = crime;
+
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getActivity(), mCrime.getTitle() + " Clicked!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         private List<Crime> mCrimes;
 
         public CrimeAdapter(List<Crime> crimes) {
@@ -73,15 +100,34 @@ public class CrimeListFragment extends Fragment {
 
         @NonNull
         @Override
-        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            return new CrimeHolder(layoutInflater, parent);
+            switch (viewType) {
+                case 0: return new CrimeHolder(layoutInflater, parent);
+                case 1: return new CrimeRequiresPoliceHolder(layoutInflater, parent);
+                default: return new CrimeHolder(layoutInflater, parent);
+            }
         }
 
         @Override
-        public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
             Crime crime = mCrimes.get(position);
-            holder.bind(crime);
+            switch (holder.getItemViewType()) {
+                case 0:
+                    CrimeHolder crimeHolder = (CrimeHolder) holder;
+                    crimeHolder.bind(crime);
+                    break;
+                case 1:
+                    CrimeRequiresPoliceHolder crimeRequiresPoliceHolder = (CrimeRequiresPoliceHolder) holder;
+                    crimeRequiresPoliceHolder.bind(crime);
+                    break;
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            Crime crime = mCrimes.get(position);
+            return crime.isRequiresPolice() ? 1 : 0;
         }
 
         @Override
